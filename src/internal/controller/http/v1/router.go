8c2250b1c3556d03252a0635dd92707b35902700
NikolaySimakov <n.simakoff@gmail.com>
2024-04-14 12:13:41 +0300

    Add logger to handlers

--- src/internal/controller/http/v1/router.go
+++ src/internal/controller/http/v1/router.go
@@ -2,16 +2,18 @@ package v1
 
 import (
 	"banner-display-service/src/internal/services"
+	"log"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func NewRouter(handler *echo.Echo, services *services.Services) {
-	// handler.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-	// 	Format: `{"time":"${time_rfc3339_nano}", "method":"${method}","uri":"${uri}", "status":${status},"error":"${error}"}` + "\n",
-	// 	Output: setLogsFile(),
-	// }))
+	handler.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `{"time":"${time_rfc3339_nano}", "method":"${method}","uri":"${uri}", "status":${status},"error":"${error}"}` + "\n",
+		Output: setLogsFile(),
+	}))
 	handler.Use(middleware.Recover())
 
 	handler.GET("/health", func(c echo.Context) error { return c.NoContent(200) })
@@ -25,4 +27,12 @@ func NewRouter(handler *echo.Echo, services *services.Services) {
 		newFeatureRoutes(v1.Group("/features"), services.Feature)
 		newBannerRoutes(v1.Group("/banners"), services.Banner)
 	}
+}
+
+func setLogsFile() *os.File {
+	file, err := os.OpenFile("logs/requests.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
 }
